8ba7b9a3b011d11ff8263839f3702e16848135b8
Angelito <73091578+Angelito08-code@users.noreply.github.com>
2026-08-03 11:55:59 +0800

    Update printdebugging

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const ExcelJS = require('exceljs');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DB_FILE = path.join(__dirname, 'database.json');
+
 
 const DB_FILE = path.join(__dirname, 'database.json');
 const ADMIN_FILE = path.join(__dirname, 'admins.json');
